2f030217a3006bf28b886787c2423a9a6eb3699c
Максим Процюк <protsuk26@gmail.com>
2025-03-20 14:55:26 +0200

    Фікс порядку питань після зміни

--- funcs/loadQuestionsToTest.js
+++ funcs/loadQuestionsToTest.js
@@ -4,7 +4,8 @@ export const loadQuestionsToTest = async id => {
   const { data, error } = await supabase
     .from('questions')
     .select('*')
-    .eq('test_id', id);
+    .eq('test_id', id)
+    .order('order', { ascending: true });
 
   if (error) {
     throw new Error(error.message);
